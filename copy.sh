a26ca8862bcb2c853bb118e0c09b5231f67d1786
Shelby Munsch <her@smuns.ch>
2016-10-04 23:38:52 -0700

    couple tweaks

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -e $HOME/.oh-my-zsh ];then
-	git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	mkdir $HOME/.oh-my-zsh/custom
 	git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/zsh-syntax-highlighting
